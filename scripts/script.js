9367b349083758b9cf1fc8322c27bc4be7401564
Cadence91 <119261807+Cadence91@users.noreply.github.com>
2022-11-27 17:34:46 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*
+  Student Name:Kyle Gonzales, Tyler cureton, Aj Harrison
+  File Name:script.js
+  Current Date:11/17/2022
+*/
+
+/* Function to display Content */
+
+function content() {
+  var text = document.getElementById("new");
+  text.textConetent = "Hi. I am a new paragraph that was created with a JavaScript funcion.";
+  text.style.color = "#c0145f";
+  text.style.fontsize = "2em";
+}
+
+
